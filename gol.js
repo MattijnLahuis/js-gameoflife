04f4d8dd870d34639be7e429a72b8d35d7ecd6db
Mattijn Lahuis <mattijnlahuis@gmail.com>
2017-06-01 12:35:00 +0200

    added function that counts the number of alive neighbors of a given cell

--- gol.js
+++ gol.js
@@ -41,7 +41,7 @@ var X = 10;
 var Y = 10;
 
 function runGame() {
-  setInterval(function() {
+  // setInterval(function() {
     var boardValues = [];
     for(var i=0; i<X; i++) {
       var row = [];
@@ -51,10 +51,46 @@ function runGame() {
         } else {
           row.push(true);
         }
-        $(`#cell-${i}-${j}`).toggleClass('alive');
+
+        console.log(i + "-" + j + " has " + numberOfNeighbors(i,j) + " neighbors");
       }
       boardValues.push(row);
     }
 
-  }, 2000)
+    for(var i=0;i<boardValues.length;i++) {
+      var row = boardValues[i];
+      for(var j=0;j<row.length;j++) {
+        var cell = row[j];
+
+        if(cell) {
+          $(`#cell-${i}-${j}`).addClass('alive');
+        } else {
+          $(`#cell-${i}-${j}`).removeClass('alive');
+        }
+      }
+    }
+
+  // }, 2000)
+}
+
+function numberOfNeighbors(x, y) {
+  var neighbors = 0;
+  //make sure we don't count the cell itself, only it's neighbors
+  if($(`#cell-${x}-${y}`).hasClass('alive')) {
+    neighbors--;
+  }
+
+
+  //to check for neighbors, we have to look around x and y 1 cell in every direction
+  for(var i=x-1; i<=x+1; i++) {
+    for(var j=y-1; j<=y+1; j++) {
+      //make sure we don't go outside of the board
+      if(i>=0 && i<X && j>=0 && j<Y) {
+        if($(`#cell-${i}-${j}`).hasClass('alive')) {
+          neighbors++;
+        }
+      }
+    }
+  }
+  return neighbors;
 }
